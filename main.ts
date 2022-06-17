c8374cfa09e306fae64bd0784ba113d6941eaa9c
keyestudio <53899501+keyestudio2019@users.noreply.github.com>
2022-06-17 13:59:02 +0800

    更新 main.ts

--- main.ts
+++ main.ts
@@ -63,8 +63,8 @@ enum LT {
 }
 
 enum LedCount {
-    Left = 12,
-    Right = 13
+    Left = 0x09,
+    Right = 0x0a
 }
 
 enum LedState {
@@ -79,7 +79,7 @@ enum Servo_num {
 
 //% color="#ff6800" icon="\uf1b9" weight=15
 //% groups="['Motor', 'Servo', 'led', 'Neo-pixel', 'Sensor', 'Tone']"
-namespace mecanumRobot {
+namespace mecanumRobotV2 {
     /**
      * use for control PCA9685
      */
@@ -88,137 +88,76 @@ namespace mecanumRobot {
         D15 = 15
     }
 
-    const PCA9685_ADDRESS = 0x47;   //device address
-    const MODE1 = 0x00;
-    let initI2C = false;
-    let _i2cError = 0;
-    let SERVOS = 0x06; // first servo address for start byte low
-    let servoTarget: number[] = [];
-    let servoActual: number[] = [];
-    let servoCancel: boolean[] = [];
-    //const MODE2 = 0x01;
-    //const SUBADR1 = 0x02;
-    //const SUBADR2 = 0x03;
-    //const SUBADR3 = 0x04;
-    const PRESCALE = 0xFE;
-    const LED0_ON_L = 0x06;
-    const LED0_ON_H = 0x07;
-    const LED0_OFF_L = 0x08;
-    //const LED0_OFF_H = 0x09;
-    //const ALL_LED_ON_L = 0xFA;
-    //const ALL_LED_ON_H = 0xFB;
-    //const ALL_LED_OFF_L = 0xFC;
-    //const ALL_LED_OFF_H = 0xFD;
+    const STC15_ADDRESS = 0x30;   //device address
 
-    let PCA9685_Initialized = false
-    // initialise the servo driver and the offset array values
-    // function initPCA(): void {
-
-    //     let i2cData = pins.createBuffer(2);
-    //     initI2C = true;
-
-    //     i2cData[0] = 0;		// Mode 1 register
-    //     i2cData[1] = 0x10;	// put to sleep
-    //     pins.i2cWriteBuffer(PCA9685_ADDRESS, i2cData, false);
-
-    //     i2cData[0] = 0xFE;	// Prescale register
-    //     i2cData[1] = 101;	// set to 60 Hz
-    //     pins.i2cWriteBuffer(PCA9685_ADDRESS, i2cData, false);
-
-    //     i2cData[0] = 0;		// Mode 1 register
-    //     i2cData[1] = 0x81;	// Wake up
-    //     pins.i2cWriteBuffer(PCA9685_ADDRESS, i2cData, false);
-
-    //     // for (let servo = 0; servo < 16; servo++) {
-    //     //     i2cData[0] = SERVOS + servo * 4 + 0;	// Servo register
-    //     //     i2cData[1] = 0x00;			// low byte start - always 0
-    //     //     _i2cError = pins.i2cWriteBuffer(PCA9685_ADDRESS, i2cData, false);
-
-    //     //     i2cData[0] = SERVOS + servo * 4 + 1;	// Servo register
-    //     //     i2cData[1] = 0x00;			// high byte start - always 0
-    //     //     pins.i2cWriteBuffer(PCA9685_ADDRESS, i2cData, false);
-
-    //     //     servoTarget[servo] = 0;
-    //     //     servoActual[servo] = 0;
-    //     //     servoCancel[servo] = false;
-    //     // }
-    //     //for (let servo = 0; servo < 16; servo++) {
-    //         i2cData[0] = SERVOS + 14 * 4 + 0;	// Servo register
-    //         i2cData[1] = 0x00;			// low byte start - always 0
-    //         _i2cError = pins.i2cWriteBuffer(PCA9685_ADDRESS, i2cData, false);
-
-    //         i2cData[0] = SERVOS + 14 * 4 + 1;	// Servo register
-    //         i2cData[1] = 0x00;			// high byte start - always 0
-    //         pins.i2cWriteBuffer(PCA9685_ADDRESS, i2cData, false);
-
-    //         servoTarget[14] = 0;
-    //         servoActual[14] = 0;
-    //         servoCancel[14] = false;
-
-    //         i2cData[0] = SERVOS + 15 * 4 + 0;	// Servo register
-    //         i2cData[1] = 0x00;			// low byte start - always 0
-    //         _i2cError = pins.i2cWriteBuffer(PCA9685_ADDRESS, i2cData, false);
-
-    //         i2cData[0] = SERVOS + 15 * 4 + 1;	// Servo register
-    //         i2cData[1] = 0x00;			// high byte start - always 0
-    //         pins.i2cWriteBuffer(PCA9685_ADDRESS, i2cData, false);
-
-    //         servoTarget[15] = 0;
-    //         servoActual[15] = 0;
-    //         servoCancel[15] = false;
-    //     //}
+    // function i2cRead(addr: number, reg: number) {
+    //     pins.i2cWriteNumber(addr, reg, NumberFormat.UInt8BE);
+    //     let val = pins.i2cReadNumber(addr, NumberFormat.UInt8BE);
+    //     return val;
     // }
-    
 
-    function i2cRead(addr: number, reg: number) {
-        pins.i2cWriteNumber(addr, reg, NumberFormat.UInt8BE);
-        let val = pins.i2cReadNumber(addr, NumberFormat.UInt8BE);
-        return val;
-    }
-
-    function i2cWrite(PCA9685_ADDRESS: number, reg: number, value: number) {
+    function i2cWrite(STC15_ADDRESS: number, reg: number, value: number) {
         let buf = pins.createBuffer(2)
         buf[0] = reg
         buf[1] = value
-        pins.i2cWriteBuffer(PCA9685_ADDRESS, buf)
+        pins.i2cWriteBuffer(STC15_ADDRESS, buf)
     }
 
-    function setFreq(freq: number): void {
-        // Constrain the frequency
-        let prescaleval = 25000000;
-        prescaleval /= 4096;
-        prescaleval /= freq;
-        prescaleval -= 1;
-        let prescale = prescaleval; //Math.Floor(prescaleval + 0.5);
-        let oldmode = i2cRead(PCA9685_ADDRESS, MODE1);
-        let newmode = (oldmode & 0x7F) | 0x10; // sleep
-        i2cWrite(PCA9685_ADDRESS, MODE1, newmode); // go to sleep
-        i2cWrite(PCA9685_ADDRESS, PRESCALE, prescale); // set the prescaler
-        i2cWrite(PCA9685_ADDRESS, MODE1, oldmode);
-        control.waitMicros(5000);
-        i2cWrite(PCA9685_ADDRESS, MODE1, oldmode | 0xa1);
-    }
 
-    function setPwm(channel: number, on: number, off: number): void {
-        let buf = pins.createBuffer(5);
-        buf[0] = LED0_ON_L + 4 * channel;
-        buf[1] = on & 0xff;
-        buf[2] = (on >> 8) & 0xff;
-        buf[3] = off & 0xff;
-        buf[4] = (off >> 8) & 0xff;
-        pins.i2cWriteBuffer(PCA9685_ADDRESS, buf);
-    }
-
-    function init_PCA9685(): void {
-        i2cWrite(PCA9685_ADDRESS, MODE1, 0x00);  //initialize the mode register 1
-        setFreq(50);   //20ms
-        for (let idx = 0; idx < 14; idx++) {
-            setPwm(idx, 0, 0);
+    /**
+     * set speed of motor
+     */
+    //% block="Motor $M run $D speed: $speed \\%"
+    //% speed.min=0 speed.max=100
+    //% group="Motor" weight=97
+    export function Motor(M: LR, D: MD, speed: number) {
+        // if (!PCA9685_Initialized) {
+        //     init_PCA9685();
+        // }
+        let speed_value = Math.map(speed, 0, 100, 0, 255);
+        //电机1
+        //正转
+        if (M == 2 && D == 1) {
+            i2cWrite(0x30, 0x01, 0); //M1A
+            i2cWrite(0x30, 0x02, speed_value); //M1B
         }
-        PCA9685_Initialized = true;
+        //反转
+        if (M == 2 && D == 0) {
+            i2cWrite(0x30, 0x01, speed_value); //M1A
+            i2cWrite(0x30, 0x02, 0); //M1B
+        }
+
+        //电机2
+        if (M == 0 && D == 0) {
+            i2cWrite(0x30, 0x03, 0); //M2A
+            i2cWrite(0x30, 0x04, speed_value); //M2B
+        }
+        if (M == 0 && D == 1) {
+            i2cWrite(0x30, 0x03, speed_value); //M2A
+            i2cWrite(0x30, 0x04, 0); //M2B
+        }
+        //电机3
+        if (M == 3 && D == 1) {
+            i2cWrite(0x30, 0x05, 0); //M3A
+            i2cWrite(0x30, 0x06, speed_value); //M3B
+        }
+        if (M == 3 && D == 0) {
+            i2cWrite(0x30, 0x05, speed_value); //M3A
+            i2cWrite(0x30, 0x06, 0); //M3B
+        }
+        //电机4
+        if (M == 1 && D == 0) {
+            i2cWrite(0x30, 0x07, 0); //M4A
+            i2cWrite(0x30, 0x08, speed_value); //M4B
+        }
+        if (M == 1 && D == 1) {
+            i2cWrite(0x30, 0x07, speed_value); //M4A
+            i2cWrite(0x30, 0x08, 0); //M4B
+        }
+
     }
 
-    
+
     /**
      * set cat state
      */
@@ -230,129 +169,28 @@ namespace mecanumRobot {
         //}
 
         if (sta == 0) {           //stop
-            setPwm(0, 0, 4095);  //control speed : 0---4095
-            setPwm(1, 0, 0);
-            setPwm(2, 0, 0);
-            setPwm(5, 0, 4095);  //control speed : 0---4095
-            setPwm(4, 0, 0);
-            setPwm(3, 0, 0);
-
-            setPwm(6, 0, 4095);  //control speed : 0---4095
-            setPwm(7, 0, 0);
-            setPwm(8, 0, 0);
-            setPwm(11, 0, 4095);  //control speed : 0---4095
-            setPwm(10, 0, 0);
-            setPwm(9, 0, 0);
+            i2cWrite(0x30, 0x01, 0); //M1A
+            i2cWrite(0x30, 0x02, 0); //M1B
+            i2cWrite(0x30, 0x03, 0); //M1A
+            i2cWrite(0x30, 0x04, 0); //M1B
+            i2cWrite(0x30, 0x05, 0); //M1A
+            i2cWrite(0x30, 0x06, 0); //M1B
+            i2cWrite(0x30, 0x07, 0); //M1A
+            i2cWrite(0x30, 0x08, 0); //M1B
         }
 
         if (sta == 1) {           //brake
-            setPwm(0, 0, 0);  //control speed : 0---4095
-            //setPwm(1, 0, 4095);
-            //setPwm(2, 0, 4095);
-            setPwm(5, 0, 0);  //control speed : 0---4095
-            //setPwm(4, 0, 4095);
-            //setPwm(3, 0, 4095);
-            setPwm(6, 0, 0);  //control speed : 0---4095
-            setPwm(11, 0, 0);  //control speed : 0---4095
+            i2cWrite(0x30, 0x01, 0); //M1A
+            i2cWrite(0x30, 0x02, 0); //M1B
+            i2cWrite(0x30, 0x03, 0); //M1A
+            i2cWrite(0x30, 0x04, 0); //M1B
+            i2cWrite(0x30, 0x05, 0); //M1A
+            i2cWrite(0x30, 0x06, 0); //M1B
+            i2cWrite(0x30, 0x07, 0); //M1A
+            i2cWrite(0x30, 0x08, 0); //M1B
         }
     }
-    /**
-     * set speed of motor
-     */
-    //% block="Motor $M run $D speed: $speed \\%"
-    //% speed.min=0 speed.max=100
-    //% group="Motor" weight=97
-    export function Motor(M: LR, D: MD, speed: number) {
-        if (!PCA9685_Initialized) {
-            init_PCA9685();
-        }
-        let speed_value = Math.map(speed, 0, 100, 0, 4095);
-        if (M == 2 && D == 1) {
-            setPwm(0, 0, speed_value);  //control speed : 0---4095
-            setPwm(1, 0, 0);
-            setPwm(2, 0, 4095);
-        }
-        if (M == 2 && D == 0) {
-            setPwm(0, 0, speed_value);  //control speed : 0---4095
-            setPwm(1, 0, 4095);
-            setPwm(2, 0, 0);
-        }
 
-        if (M == 0 && D == 0) {
-            setPwm(5, 0, speed_value);  //control speed : 0---4095
-            setPwm(4, 0, 0);
-            setPwm(3, 0, 4095);
-        }
-        if (M == 0 && D == 1) {
-            setPwm(5, 0, speed_value);  //control speed : 0---4095
-            setPwm(4, 0, 4095);
-            setPwm(3, 0, 0);
-        }
-
-        if (M == 3 && D == 1) {
-            setPwm(6, 0, speed_value);  //control speed : 0---4095
-            setPwm(7, 0, 0);
-            setPwm(8, 0, 4095);
-        }
-        if (M == 3 && D == 0) {
-            setPwm(6, 0, speed_value);  //control speed : 0---4095
-            setPwm(7, 0, 4095);
-            setPwm(8, 0, 0);
-        }
-
-        if (M == 1 && D == 0) {
-            setPwm(11, 0, speed_value);  //control speed : 0---4095
-            setPwm(10, 0, 0);
-            setPwm(9, 0, 4095);
-        }
-        if (M == 1 && D == 1) {
-            setPwm(11, 0, speed_value);  //control speed : 0---4095
-            setPwm(10, 0, 4095);
-            setPwm(9, 0, 0);
-        }
-
-    }
-    
-    //% block="set servo to angle %angle"
-    //% group="Servo" weight=70
-    //% angle.min=-90 angle.max.max=90
-    export function setServo(angle: number): void {
-        pins.servoWritePin(AnalogPin.P14, angle)
-    }
-
-    
-    /**
-      * Set Servo Position by Angle
-      * @param servo Servo number (0 to 15)
-      * @param angle degrees to turn servo (-90 to +90)
-      */
-    // export function setServo(Servo: Servos, angle: number): void {
-    //     setServoRaw(Servo, angle);
-    //     //servoTarget[Servo] = angle;
-    // }
-
-    // function setServoRaw(servo: number, angle: number): void {
-    //     if (initI2C == false) {
-    //         initPCA();
-    //     }
-    //     // two bytes need setting for start and stop positions of the servo
-    //     // servos start at SERVOS (0x06) and are then consecutive blocks of 4 bytes
-    //     // the start position (always 0x00) is set during init for all servos
-
-    //     let i2cData = pins.createBuffer(2);
-    //     let start = 0;
-    //     angle = Math.max(Math.min(90, angle), -90);
-    //     let stop = 369 + angle * 223 / 90;
-
-    //     i2cData[0] = SERVOS + servo * 4 + 2;	// Servo register
-    //     i2cData[1] = (stop & 0xff);		// low byte stop
-    //     pins.i2cWriteBuffer(PCA9685_ADDRESS, i2cData, false);
-
-    //     i2cData[0] = SERVOS + servo * 4 + 3;	// Servo register
-    //     i2cData[1] = (stop >> 8);		// high byte stop
-    //     pins.i2cWriteBuffer(PCA9685_ADDRESS, i2cData, false);
-    //     servoActual[servo] = angle;
-    // }
 
     /**
      * turn off all rgb-led
@@ -360,11 +198,18 @@ namespace mecanumRobot {
     //% block="$LedC Colorful LED turn $LedS"
     //% group="led" weight=76
     export function setLed(LedC: LedCount, LedS: LedState) {
-        if (!PCA9685_Initialized) {
-            init_PCA9685();
-        }
-        setPwm(LedC, 0, LedS);
+        i2cWrite(0x30, LedC, LedS);
     }
+
+    //% block="set servo to angle %angle"
+    //% group="Servo" weight=70
+    //% angle.min=-90 angle.max.max=90
+    export function setServo(angle: number): void {
+        pins.servoWritePin(AnalogPin.P14, angle)
+    }
+
+
+
 
     /////////////////////////////////////////////////////
     //% block="$LT_val LineTracking"
@@ -372,11 +217,11 @@ namespace mecanumRobot {
     export function LineTracking(LT_val: LT) {
         let val = 0;
         let lt = LT_val;
-        switch(lt){
-            case LT.Left  :
+        switch (lt) {
+            case LT.Left:
                 val = pins.digitalReadPin(DigitalPin.P1);
                 break;
-            case LT.Right :
+            case LT.Right:
                 val = pins.digitalReadPin(DigitalPin.P2);
                 break;
         }
@@ -412,5 +257,5 @@ namespace mecanumRobot {
 
         return Math.round(ret / 58);
     }
-    
+
 }
